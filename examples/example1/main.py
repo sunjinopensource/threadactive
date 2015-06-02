6494441eb97cbf8202f60712aac29cb7c6cdbfce
sunjin <sunjinopensource@qq.com>
2015-06-02 11:15:33 +0800

    + example1

---
+++ examples/example1/main.py
@@ -0,0 +1,31 @@
+import threadactive
+import time
+import os
+import threading
+
+class BackWorker(threadactive.Agent):
+    def tick(self):
+        threadactive.Agent.tick(self)
+        print("[%s][%d] front" % (threading.current_thread().getName(), time.clock()) )
+        self.print_in_back()
+        time.sleep(1)
+
+    @threadactive.backend
+    def print_in_back(self, *args, **kwargs):
+        print("[%s][%d] back" % (threading.current_thread().getName(), time.clock()) )
+        if time.clock() > 3:
+            self.back_to_front()
+
+    @threadactive.frontend
+    def back_to_front(self, *args, **kwargs):
+        print("[%s][%d] back to front" % (threading.current_thread().getName(), time.clock()) )
+
+
+def main():
+    bw = BackWorker()
+    while True:
+        bw.tick()
+
+
+if __name__ == '__main__':
+    main()
